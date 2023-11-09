89364326b611338e5b07393adc70c7b7e32bdd59
SiddharthSoora <77402814+SiddharthSoora@users.noreply.github.com>
2023-11-09 16:17:25 +0530

    frontend ui

---
+++ frontend/src/app/components/titlebar.js
@@ -0,0 +1,8 @@
+
+export default function TitleBar() {
+  return (
+    <div className="bg-white opacity-50 text-center rounded-lg p-4 w-1/2 mx-auto">
+      <h1 className="text-black font-bold text-2xl">Generate Questions Here!</h1>
+    </div>
+  );
+};
